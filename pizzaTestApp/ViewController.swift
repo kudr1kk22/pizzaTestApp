9731d96301f67a19b32b05172188cc93df758d8b
kudr1kk22 <kudr1kk22@gmail.com>
2023-04-02 23:23:40 +0300

    Initial Commit

---
+++ pizzaTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pizzaTestApp
+//
+//  Created by Eugene Kudritsky on 2.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
